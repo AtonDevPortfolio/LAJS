7d24dc911c2b6634c049c4542c8bcaf8d061e914
anton-b117 <anton@maroonspear.com>
2015-05-18 09:10:03 -0400

    completed index.html and app.js.

---
+++ chap2_siteroot/js/app.js
@@ -0,0 +1,23 @@
+// Chapter2/app.js
+
+'use strict';
+// App Module
+
+var helloWorldApp = angular.module('helloWorldApp', [
+	'ngRoute',
+	'helloWorldControllers'
+]);
+
+helloWorldApp.config(['$routeProvider', '$locationProvider',
+function($routeProvider, locationProvider) {
+	$routeProvider.
+		when('/', {
+		templateUrl: 'partials/main.html',
+		controller: 'MainCtrl'
+	}).when('/show', {
+		templateUrl: 'partials/show.html',
+		controller: 'ShowCtrl'
+});
+
+$locationProvider.html5mode(false).hashPrefix('!');
+}]);
